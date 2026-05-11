2144bd01c5fed866db4b4d949e5553abe9779d02
Wahyu Nurhidayat <60255281+waynh77@users.noreply.github.com>
2026-05-11 21:01:25 +0700

    Add Customer model, update Product, and improve navigation
    Added Customer model and view for displaying customers. Updated Product model to include Qty property and adjusted Product view. Moved product list from Index to Product view. Enhanced navigation with links to Product and Customer pages.

--- Sesi 1/Controllers/HomeController.cs
+++ Sesi 1/Controllers/HomeController.cs
@@ -15,16 +15,32 @@ namespace Sesi_1.Controllers
 
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Customer()
+        {
+            var customer = new List<Customer>
+        {
+            new Customer { Id = 1, Name = "Alpha",Address="Jl A" },
+            new Customer { Id = 2, Name = "Beta",Address="Jl B" },
+            new Customer { Id = 3, Name = "Charlie",Address="Jl C" },
+            new Customer { Id = 4, Name = "Delta",Address="Jl D" },
+        };
+            return View(customer);
+        }
+
+        public IActionResult Product()
+        {
             var products = new List<Product>
         {
-            new Product { Id = 1, Name = "Laptop" },
-            new Product { Id = 2, Name = "Mouse" },
-            new Product { Id = 3, Name = "Monitor" },
-            new Product { Id = 4, Name = "Keyboard" },
+            new Product { Id = 1, Name = "Laptop",Qty=10 },
+            new Product { Id = 2, Name = "Mouse",Qty=5 },
+            new Product { Id = 3, Name = "Monitor",Qty=7 },
+            new Product { Id = 4, Name = "Keyboard",Qty=6 },
         };
             return View(products);
         }
-
         public IActionResult Privacy()
         {
             return View();
--- Sesi 1/Models/Customer.cs
+++ Sesi 1/Models/Customer.cs
@@ -1,9 +1,9 @@
 ﻿namespace Sesi_1.Models
 {
-    public class Product
+    public class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Qty { get; set; }
+        public string Address { get; set; }
     }
 }
